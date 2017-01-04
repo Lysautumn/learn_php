f2bf62730adc9afc77428940f6499b9338497d38
Ally Boyd <alyssa.boyd85@gmail.com>
2017-01-04 16:03:59 -0600

    Learning PHP objects

--- index.php
+++ index.php
@@ -122,5 +122,30 @@
 	    print strtoupper($winner);
 	?>
     </p>
+		<p>
+			<?php
+			// PHP objects!
+        class Person {
+            public $isAlive = true;
+            public $firstname;
+            public $lastname;
+            public $age;
+            public function __construct($firstname, $lastname, $age){
+                $this->firstname = $firstname;
+                $this->lastname = $lastname;
+                $this->age = $age;
+            }
+            public function greet(){
+                return "Hello, my name is " . $this->firstname . " " . $this->lastname . ". Nice to meet you! :-)";
+            }
+        }
+        $teacher = new Person("boring", "12345", 12345);
+        $student = new Person("ally", "boyd", 31);
+        echo $teacher->isAlive;
+        echo $student->age;
+        echo $teacher->greet();
+        echo $student->greet();
+        ?>
+		</p>
 	</body>
 </html>
